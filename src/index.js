f04abe5a05e8fd1011018aa5939872fb876e96e8
ostuxcat <ostuxcat@gmail.com>
2025-10-16 21:01:07 +0530

    Meow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+  .then(
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Is Running On PORT: ${process.env.PORT}`);
+    })
+  )
+  .catch((err) => {
+    console.log("MongoDB Connection Failed!", err);
+  });
